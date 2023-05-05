fd2e10ec3f3dde62c3389d52a1956d258a8d7891
idk-v1 <104273594+idk-v1@users.noreply.github.com>
2023-05-05 15:58:12 -0500

    theme changing

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-var dark = localStorage.getItem("theme") === "dark";
+var dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+if (localStorage.getItem("theme") === "dark")
+    dark = true;
+if (localStorage.getItem("theme") === "light")
+    dark = false;
+
 document.querySelector("link").href = (dark ? "dark.css" : "light.css");
+
+function toggleTheme()
+{
+    dark = !dark;
+    localStorage.setItem("theme", (dark ? "dark" : "light"));
+    document.querySelector("link").href = (dark ? "dark.css" : "light.css");
+}
